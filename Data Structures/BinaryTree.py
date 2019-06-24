3b9e720c87bedb9c409d754742963bfa33ce7ef0
Christian Miljkovic <cmm892@stern.nyu.edu>
2019-06-24 16:04:44 -0400

    Completed insert and pre-order methods for a Binary Tree

--- Data Structures/BinaryTree.py
+++ Data Structures/BinaryTree.py
@@ -4,32 +4,46 @@ class BinaryTree:
 
     def __init__(self):
         self.root = None
-    
+        self.preOrderList = []
+
     def insert(self, data):
         if(data == None):
             return
         else:
-            self.insertInternal(self.root, data)
+            if(self.root == None):
+                self.root = Node(data)
+            else:
+                newNode = Node(data)
+                self.insertInternal(self.root, newNode)
 
-    def insertInternal(self, node, data):
-        if(node == None):
-            node = Node(data)
-            print(node)
+    def insertInternal(self, root, node):
+        if(root == None):
+            root = node
         else:
-            if(node.data >= data):
-                self.insertInternal(node.left, data)
-            elif(node.data < data):
-                self.insertInternal(node.right, data)
+            if(root.data >= node.data):
+                if(root.left is None):
+                    root.left = node
+                else:
+                    self.insertInternal(root.left, node)
+            else:
+                if(root.right is None):
+                    root.right = node
+                else:
+                    self.insertInternal(root.right, node)
+
 
     def toString(self):
-        return self.preOrder(self.root)
+        self.preOrder(self.root)
+        return self.preOrderList
     
     def preOrder(self, node):
-        preOrderList = []
         if(node != None):
-            preOrderList.append(node.data)
+            # to do in-order or post-order you just move the append either in the middle
+            # of the node.left and node.right calls or at the end of all the recursive calls
+            self.preOrderList.append(node.data)
             self.preOrder(node.left)
             self.preOrder(node.right)
+            
 
 class Node:
 
@@ -42,9 +56,10 @@ class Node:
 testBTree = BinaryTree()
 testBTree.insert(5)
 testBTree.insert(3)
-testBTree.insert(7)
+testBTree.insert(10)
 testBTree.insert(1)
 testBTree.insert(2)
-testBTree.insert(6)
-testBTree.insert(8)
+testBTree.insert(16)
+testBTree.insert(18)
+testBTree.insert(14)
 print(testBTree.toString())
