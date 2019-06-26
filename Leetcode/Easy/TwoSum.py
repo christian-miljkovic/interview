ccc6aad2d064be0b593fa299ff295883c324814a
Christian Miljkovic <cmm892@stern.nyu.edu>
2019-06-26 00:31:21 -0400

    Created directory for Leetcode problems and solved an easy TwoSum problem

---
+++ Leetcode/Easy/TwoSum.py
@@ -0,0 +1,30 @@
+'''
+Description: 
+
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+'''
+
+# Brute Force Approach
+# Time Complexity: O(n^2)
+# Space Complexity O(1)
+def twoSum(self, nums, target):
+    for i in range(0,len(nums)):
+        for j in range(1,len(nums)):
+            if nums[i] + nums[j] == target and i != j :
+                return [i,j]
+
+# Two Pass Hash Table
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+def twoSumHashTwo(self, nums, target):
+    hashTable = dict()
+    for i in range(0,len(nums)):
+        hashTable[nums[i]] = i
+    
+    for j in range(0, len(nums)):
+        difference = target - nums[j]
+        if(difference in hashTable):
+            if(hashTable[difference] != j):
+                return [j, hashTable[difference]]
