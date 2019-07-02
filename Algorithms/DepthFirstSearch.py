ef23b83fe3e9e7be86229141af34bab5e83ddd64
Christian Miljkovic <cmm892@stern.nyu.edu>
2019-07-02 17:02:44 -0400

    Finished implementing DFS on a graph represented by an Adjaceny List

---
+++ Algorithms/DepthFirstSearch.py
@@ -0,0 +1,38 @@
+# Depth first search of a graph represented as Adjacency List
+
+class AdjacencyList:
+
+    def __init__(self):
+        self.graph = dict()
+    
+
+    def addNode(self, vertex, *edges):
+        listOfEdges = []
+        for edge in edges:
+            listOfEdges.append(edge)
+        
+        self.graph[vertex] = listOfEdges
+    
+
+    def depthFirstTraversal(self, vertexNumber):
+
+        # Do not need to have bool to indicate whether the vertex has been visited 
+        # Assume that the graph is a DAG (directed acyclic graph)
+        print(vertexNumber)
+
+        # If the vertex number doesn't exist in the graph then we know it is a leaf vertex
+        if(vertexNumber not in self.graph.keys()):
+            return 
+        
+        listOfEdges = self.graph[vertexNumber]
+
+        for child in listOfEdges:
+            self.depthFirstTraversal(child)
+
+if __name__ == "__main__":
+    adjList = AdjacencyList()
+    adjList.addNode(0,4,7,6)
+    adjList.addNode(4,2,1)
+    adjList.addNode(7,8,5)
+    adjList.addNode(6,3)
+    adjList.depthFirstTraversal(0)
