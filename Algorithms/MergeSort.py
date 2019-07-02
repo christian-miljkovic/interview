23c8a0c8a6c72d1f5bb468038e82598991e68025
Christian Miljkovic <cmm892@stern.nyu.edu>
2019-07-02 14:03:23 -0400

    Fully implemented merge sort algorithm for python lists

--- Algorithms/MergeSort.py
+++ Algorithms/MergeSort.py
@@ -1,13 +1,59 @@
 # Mergesort algorithm for sorting a list
 import random
 
+def mergeSort(arrayToBeSorted):
+
+    if(len(arrayToBeSorted)>1):
+
+        midPoint = len(arrayToBeSorted)//2
+        leftArray = arrayToBeSorted[:midPoint]
+        rightArray = arrayToBeSorted[midPoint:]
+
+        mergeSort(leftArray)
+        mergeSort(rightArray)
+
+        # these indicies are used for the two arrays being merged
+        i = 0
+        j = 0
+
+        # this index is used for the new array
+        k = 0
+
+        while(i < len(leftArray) and j < len(rightArray)):
+            if(leftArray[i] <= rightArray[j]):
+                arrayToBeSorted[k] = leftArray[i]
+                i += 1
+            else:
+                arrayToBeSorted[k] = rightArray[j]
+                j += 1
+            k += 1
+        
+        # check to make sure no elements were left
+        while(i < len(leftArray)):
+            arrayToBeSorted[k] = leftArray[i]
+            i += 1
+            k += 1
+        while(j < len(rightArray)):
+            arrayToBeSorted[k] += rightArray[j]
+            j += 1
+            k += 1
+
+if __name__ == '__main__':
+    unsortedTestList = random.sample(range(0,100),10)
+    print(unsortedTestList)
+    mergeSort(unsortedTestList)
+    print(unsortedTestList)
+
+
+"""
+Keeping previous attempt in order to understand why/how I implemented this incorrectly
+
 def mergeSort(array, lowIndex, highIndex):
 
     if(len(array) > 1):
         mid = len(array)//2
-        leftArray = array[lowIndex:mid]
-        rightArray = array[mid:highIndex]
-        print(leftArray, rightArray)
+        leftArray = array[:mid]
+        rightArray = array[mid:]
         arrayOne = mergeSort(leftArray, lowIndex, mid)
         arrayTwo = mergeSort(rightArray, mid, highIndex)
         return mergeArrays(arrayOne, arrayTwo)
@@ -30,9 +76,4 @@ def mergeArrays(arrayOne, arrayTwo):
             indexTwo += 1
     
     return mergedArray
-
-if __name__ == '__main__':
-    unsortedTestList = random.sample(range(0,100),10)
-    print(unsortedTestList)
-    mergeSort(unsortedTestList, 0, len(unsortedTestList))
-    print(unsortedTestList)
+"""
