4bf699f0138484986f1c6b73d352dcfbfeba3af6
Christian Miljkovic <cmm892@stern.nyu.edu>
2019-07-22 17:24:25 -0400

    Implemented a search similar to merge sort logic

---
+++ CrackingCodingInterview/SortingAndSearching/SearchRotatedArray.py
@@ -0,0 +1,29 @@
+"""
+Chapter 10 - Sorting and Searching
+Problem - Search in Rotated Array: Given a sorted array of n intergers that has been rotated an unknown number
+of times, write code to find an element in the array. You may assume that the array was originally sorted in increasing order.
+Time Complexity: O(log(n))
+"""
+
+def shiftedBinarySearch(array, start, end, element):
+    if start < end:
+        midPoint = (start + end)//2
+        if array[midPoint] == element:
+            return midPoint
+        elif array[start] == element:
+            return start
+        elif array[end] == element:
+            return end
+        else:
+            rightIndex = shiftedBinarySearch(array, midPoint + 1, end, element)
+            leftIndex = shiftedBinarySearch(array, start, midPoint - 1, element)
+            if rightIndex != None:
+                return rightIndex
+            elif leftIndex != None:
+                return leftIndex
+            else:
+                return None
+
+if __name__ == "__main__":
+    arr = [5,6,0,2,3,4]
+    print(shiftedBinarySearch(arr,0,len(arr)-1,6))
