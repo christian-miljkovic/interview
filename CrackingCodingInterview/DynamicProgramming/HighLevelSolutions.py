47735f146c11fde8c2acad18803c5f8d31ff0690
Christian Miljkovic <cmm892@stern.nyu.edu>
2019-07-30 18:18:35 -0400

    Adding high level solutions for this chapter

---
+++ CrackingCodingInterview/DynamicProgramming/HighLevelSolutions.py
@@ -0,0 +1,12 @@
+"""
+Chapter 8 - Dynamic Programming
+These are high level solutions that I created for the rest of the practice problems in this chapter
+"""
+
+# 8.4 Power Set
+"""
+Problem: Write a method to return all subsets of a set
+
+Solution: 
+
+"""
